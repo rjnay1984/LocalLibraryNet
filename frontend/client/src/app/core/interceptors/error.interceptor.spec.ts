3880073e5eb049fccfd0b6973f25011660f48377
RJ Nay <rjnay1984@gmail.com>
2021-02-16 15:30:43 -0600

    update interceptor spec

--- frontend/client/src/app/core/interceptors/error.interceptor.spec.ts
+++ frontend/client/src/app/core/interceptors/error.interceptor.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ErrorInterceptor } from './error.interceptor';
 
 describe('ErrorInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule, MatSnackBarModule],
     providers: [
       ErrorInterceptor
-      ]
+    ]
   }));
 
   it('should be created', () => {
