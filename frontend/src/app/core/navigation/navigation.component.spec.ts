2cbc5d3711d62621437f58f25852c0023b25e6db
RJ Nay <rjnay1984@gmail.com>
2021-02-24 22:37:39 -0600

    enhance nav component testing

--- frontend/src/app/core/navigation/navigation.component.spec.ts
+++ frontend/src/app/core/navigation/navigation.component.spec.ts
@@ -1,10 +1,9 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatMenuModule } from '@angular/material/menu';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MaterialModule } from 'src/app/shared/material.module';
-import { SharedModule } from 'src/app/shared/shared.module';
 
+import { User } from '../models/user';
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
@@ -26,5 +25,30 @@ describe('NavigationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show login/register when no currentUser$', () => {
+    expect(fixture).toMatchSnapshot();
+  });
+
+  it('should show email when currentUser$', () => {
+    const user = {
+      token:
+        // eslint-disable-next-line max-len
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImRlbW91c2VyQG1pY3Jvc29mdC5jb20iLCJyb2xlIjoiTWVtYmVycyIsIm5iZiI6MTYxMzk2NjAzMywiZXhwIjoxNjE0NTcwODMzLCJpYXQiOjE2MTM5NjYwMzN9.eT32LoOdwK2Eelk64mbUdRaw69156Yihwpv_8yLCA7o',
+      username: 'user',
+      roles: ['role1'],
+    };
+
+    component.accountService.setCurrentUser(user);
+    let currentUser: User;
+    fixture.detectChanges();
+
+    component.accountService.currentUser$.subscribe((response) => {
+      currentUser = response;
+    });
+
+    expect(currentUser.username).toEqual('demouser@microsoft.com');
+    expect(fixture).toMatchSnapshot();
   });
 });
